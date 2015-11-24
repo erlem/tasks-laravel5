91933a112f6b1c4c8cbfe9b30b5fc53489b34dc1
DALEX Tanguy <dalex.tanguy@gmail.com>
2015-11-24 10:07:30 +0100

    ajout de test unitaire

--- tests/Http/Controllers/TasksControllerTest.php
+++ tests/Http/Controllers/TasksControllerTest.php
@@ -16,4 +16,41 @@ class TasksControllerTest extends TestCase
         $response = $this->action('GET', 'TasksController@index');
         $this->assertResponseOk();
     }
+    public function testStore()
+    {
+    $this->visit('tasks/create')
+         ->type('newTitle', 'title')
+         ->type('newDescription', 'description')
+         ->press('Create New Task')
+         ->seePageIs('/tasks');
+        $this->assertRedirectedToRoute('tasks.index');
+    
+    }
+    public function testShow()
+    {
+       $response = $this->action('GET', 'TasksController@show', ['tasks' => 1]);
+        $this->assertResponseOk();
+    }
+    public function testEdit()
+    {
+        $response = $this->action('GET', 'TasksController@edit', ['tasks' => 1]);
+        $this->assertResponseOk();
+    }
+    public function testUpdate()
+    {
+        $this->visit('tasks/1/edit')
+         ->type('newTitle', 'title')
+         ->type('newDescription', 'description')
+         ->press('Update Task')
+         ->seePageIs('/tasks');
+        $this->assertRedirectedToRoute('tasks.index');
+    }
+    public function testDestroy()
+    {
+        $this->visit('tasks')
+         ->press('Delete this task?')
+         ->seePageIs('/tasks');
+        $this->assertRedirectedToRoute('tasks.index');
+    }
+    
 }
