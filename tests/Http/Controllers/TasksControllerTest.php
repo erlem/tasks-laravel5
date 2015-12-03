a5087d94e9322544af4e63ffecdea9048d893528
Marchandise Rudy <r.marchandise@sfr.fr>
2015-12-03 16:31:46 +0100

    Ajout des tests unitaires
    PhpUnit : CRUD - TasksControllerTest.php

--- tests/Http/Controllers/TasksControllerTest.php
+++ tests/Http/Controllers/TasksControllerTest.php
@@ -16,4 +16,47 @@ class TasksControllerTest extends TestCase
         $response = $this->action('GET', 'TasksController@index');
         $this->assertResponseOk();
     }
+
+    /*
+     * Create new task
+     * @return void
+     */
+    public function testCreate(){
+      $this->visit('/')
+      ->click('Add New Task')
+      ->seePageIs('/tasks/create');
+    }
+
+    /**
+     * Update task (edit button)
+     * @return void
+     */
+    public function testUpdate(){
+      $this->visit('/tasks/1/edit')
+      ->type('New title', 'title')
+      ->type('New Desc', 'description')
+      ->press('Update Task')
+      ->seePageIs('/tasks');
+    }
+
+    /**
+     * Delete task ans back to listing
+     * @return void
+     */
+    public function testDestroy(){
+      $this->visit('tasks/1')
+      ->press('Delete this task?')
+      ->seePageIs('/tasks');
+    }
+
+    /**
+    * Show task and back listing
+    * @return void
+    */
+    public function testShow(){
+      $this->visit('/tasks')
+      ->click('View Task')
+      ->click('Back to all tasks')
+      ->seePageIs('/tasks');
+    }
 }
