15cc1720d68aec5452a83ddd04a4cf32634e9de2
callot.antoine <callot.antoine@gmail.com>
2015-12-06 14:35:02 +0100

    fonction non utiliser commenter

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -37,8 +37,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function map(Router $router)
     {
-        $router->group(['namespace' => $this->namespace], function ($router) {
+        /**$router->group(['namespace' => $this->namespace], function ($router) {
             require app_path('Http/routes.php');
-        });
+        });*/
     }
 }
